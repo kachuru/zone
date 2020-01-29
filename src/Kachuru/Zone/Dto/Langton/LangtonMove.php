7bcee0e71c608a83338bf99a87bfb745061e02bc
Rupert Jones <ru@kachuru.uk>
2020-01-29 13:22:21 +0000

    Add PHPStan to the project

--- src/Kachuru/Zone/Dto/Langton/LangtonMove.php
+++ src/Kachuru/Zone/Dto/Langton/LangtonMove.php
@@ -43,14 +43,7 @@ class LangtonMove implements \JsonSerializable
     {
         return $this->updateTileState;
     }
-
-    /**
-     * Specify data which should be serialized to JSON
-     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     * which is a value of any type other than a resource.
-     * @since 5.4.0
-     */
+    
     public function jsonSerialize(): array
     {
         return [
