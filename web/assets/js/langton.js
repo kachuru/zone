ac4b1a54cdb5376fbf977604aabfd1c527686123
Rupert Jones <ru@kachuru.uk>
2021-03-18 14:11:43 +0000

    Tidy up the JS a bit

--- web/assets/js/langton.js
+++ web/assets/js/langton.js
@@ -2,42 +2,60 @@ function getTileState(tileId) {
     return ($('#tile-' + tileId + '-outer').attr('class').split(' ')[1]).split('-')[2];
 }
 
-function langtonMove(moves) {
-    moves++;
+function updateTileState(tileId, newState) {
+    let oldTileOuter = '#tile-' + tileId + '-outer';
+
+    $(oldTileOuter).removeClass(function (index, className) {
+        return (className.match(/(^|\s)tile-state-\S+/g) || []).join(' ');
+    });
+    $(oldTileOuter).addClass('tile-state-' + newState);
+}
+
+function updateAntState(oldTileId, newTileId, antOrientation) {
+    let oldTileInner = '#tile-' + oldTileId + '-inner';
+    let newTileInner = '#tile-' + newTileId + '-inner';
+
+    $(oldTileInner).removeClass('ant');
+    $(oldTileInner).removeClass(function (index, className) {
+        return (className.match(/(^|\s)ant-orient-\S+/g) || []).join(' ');
+    });
+    $(newTileInner).addClass('ant');
+    $(newTileInner).addClass('ant-orient-' + antOrientation);
+}
+
+function updateSummaryBox(newTileId, antOrientation, numberOfMoves) {
+    $('#ant-location-tile-id').text(newTileId);
+    $('#ant-location-orientation').text(antOrientation);
+    $('#ant-steps-taken').text(numberOfMoves);
+}
+
+function langtonMove(steps) {
+    steps++;
+
+    let antLocation = $('#ant-location-tile-id').text();
+    let seedId = $('#seed-id').text();
+    let antOrientation = $('#ant-location-orientation').text();
 
     $.post(
         "/langton/move",
         {
-            'seedId': $('#seed-id').text(),
-            'tileId': $('#ant-location-tile-id').text(),
-            'orientation': $('#ant-location-orientation').text(),
-            'state': getTileState($('#ant-location-tile-id').text())
+            'seedId': seedId,
+            'tileId': antLocation,
+            'orientation': antOrientation,
+            'state': getTileState(antLocation)
         },
         function (response) {
-            var oldTileId = '#tile-' + $('#ant-location-tile-id').text() + '-inner';
-            var oldTileOuter = '#tile-' + $('#ant-location-tile-id').text() + '-outer';
-            var newTileId = '#tile-' + response.antStateLocation + '-inner';
+            let oldTileId = $('#ant-location-tile-id').text();
+            let newTileId = response.antStateLocation;
+            let stepsLimit = $('#ant-steps-limit').text();
 
-            $(oldTileId).removeClass('ant');
-            $(oldTileId).removeClass(function (index, className) {
-                return (className.match(/(^|\s)ant-orient-\S+/g) || []).join(' ');
-            });
+            updateAntState(oldTileId, newTileId, response.antStateOrientation);
+            updateTileState(oldTileId, response.updateTileState);
+            updateSummaryBox(newTileId, response.antStateOrientation, steps);
 
-            $(oldTileOuter).removeClass(function (index, className) {
-                return (className.match(/(^|\s)tile-state-\S+/g) || []).join(' ');
-            });
-
-            $(newTileId).addClass('ant');
-            $(newTileId).addClass('ant-orient-' + response.antStateOrientation);
-            $(oldTileOuter).addClass('tile-state-' + response.updateTileState);
-
-            $('#ant-location-tile-id').text(response.antStateLocation);
-            $('#ant-location-orientation').text(response.antStateOrientation);
-            $('#ant-steps-taken').text(moves);
-
-            if (moves <= $('#ant-steps-limit').text()) {
+            if (steps <= stepsLimit) {
                 setTimeout(function () {
-                    langtonMove(moves);
+                    langtonMove(steps);
                 }, 500);
             }
         }
